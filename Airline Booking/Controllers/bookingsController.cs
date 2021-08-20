6f9046d1e46cada4a287880d6cf6214fa5ac1c3c
Naren0225 <narendranaren881@gmail.com>
2021-08-20 08:05:18 +0530

    Add project files.

---
+++ Airline Booking/Controllers/bookingsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airline_Booking.Controllers
+{
+    public class bookingsController : Controller
+    {
+        private airlines_dbEntities db = new airlines_dbEntities();
+
+        // GET: bookings
+        public ActionResult Index()
+        {
+            var bookings = db.bookings.Include(b => b.flight).Include(b => b.user);
+            return View(bookings.ToList());
+        }
+
+        // GET: bookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            booking booking = db.bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // GET: bookings/Create
+        public ActionResult Create()
+        {
+            ViewBag.fid = new SelectList(db.flights, "fid", "fname");
+            ViewBag.uid = new SelectList(db.users, "uid", "uname");
+            return View();
+        }
+
+        // POST: bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,fid,uid,qty,card_number,cvv,exp_mnth_year,booking_date")] booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.bookings.Add(booking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.fid = new SelectList(db.flights, "fid", "fname", booking.fid);
+            ViewBag.uid = new SelectList(db.users, "uid", "uname", booking.uid);
+            return View(booking);
+        }
+
+        // GET: bookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            booking booking = db.bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.fid = new SelectList(db.flights, "fid", "fname", booking.fid);
+            ViewBag.uid = new SelectList(db.users, "uid", "uname", booking.uid);
+            return View(booking);
+        }
+
+        // POST: bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,fid,uid,qty,card_number,cvv,exp_mnth_year,booking_date")] booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(booking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.fid = new SelectList(db.flights, "fid", "fname", booking.fid);
+            ViewBag.uid = new SelectList(db.users, "uid", "uname", booking.uid);
+            return View(booking);
+        }
+
+        // GET: bookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            booking booking = db.bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            booking booking = db.bookings.Find(id);
+            db.bookings.Remove(booking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
